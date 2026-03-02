d666e0ccefc57a4546f52081e7e9039fb429ec17
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-03-02 21:19:38 +0530

    add low-pass filter

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -41,10 +41,30 @@ for s in df["set"].unique():
     df.loc[(df["set"] == s), "duration"] = duration.seconds
 
 duration_df = df.groupby(["category"])["duration"].mean()
+
+duration_df.iloc[0] / 5
+duration_df.iloc[1] / 10
     
 # --------------------------------------------------------------
 # Butterworth lowpass filter
 # --------------------------------------------------------------
+
+df_lowpass = df.copy()
+LowPass = LowPassFilter()
+
+fs = 1000/200
+cutoff = 1.3
+
+df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
+
+subset = df_lowpass[df_lowpass["set"] == 45]
+print(subset["label"].iloc[0])
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+ax[0].plot(subset["acc_y"].reset_index(drop=True), label="raw data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop=True), label="butterworth filter")
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
 
 
 # --------------------------------------------------------------
