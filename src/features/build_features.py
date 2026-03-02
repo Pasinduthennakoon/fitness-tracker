28c66939670713fb9bf2922c91a0eb09a8b4417b
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-03-02 20:43:12 +0530

    Update build_features.py

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -8,18 +8,39 @@ from TemporalAbstraction import NumericalAbstraction
 # --------------------------------------------------------------
 # Load data
 # --------------------------------------------------------------
+df = pd.read_pickle('../../data/interim/02_outlier_removed_Chauvenets.pkl')
 
+predictor_column = list(df.columns[:6])
 
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
 # --------------------------------------------------------------
 # Dealing with missing values (imputation)
 # --------------------------------------------------------------
+for col in predictor_column:
+    df[col] = df[col].interpolate()
 
-
+df.info()
 # --------------------------------------------------------------
 # Calculating set duration
 # --------------------------------------------------------------
 
+df[df["set"] == 25]["acc_y"].plot()
+df[df["set"] == 50]["acc_y"].plot()
 
+duration = df[df["set"] == 1].index[-1] - df[df["set"] == 1].index[0]
+duration.seconds
+
+for s in df["set"].unique():
+    start = df[df["set"] == 1].index[0]
+    stop = df[df["set"] == 1].index[-1]
+    
+    duration = stop - start
+    df.loc[(df["set"] == s), "duration"] = duration.seconds
+
+    
 # --------------------------------------------------------------
 # Butterworth lowpass filter
 # --------------------------------------------------------------
